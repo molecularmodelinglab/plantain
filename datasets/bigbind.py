3e37f8fa15cc3df196b481c17ad07a18b1ab8b7f
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-09-26 14:23:41 -0400

    added ki/kd option

--- datasets/bigbind.py
+++ datasets/bigbind.py
@@ -16,3 +16,5 @@ class BigBindDataset(CacheableDataset):
         max_pocket_size=42
         # todo: we need to filter like this beforehand
         self.activities = self.activities.query("num_pocket_residues >= 5 and pocket_size_x < @max_pocket_size and pocket_size_y < @max_pocket_size and pocket_size_z < @max_pocket_size").reset_index(drop=True)
+        if cfg.data.only_ki_kd:
+            self.activities = self.activities.query("standard_type == 'Ki' or standard_type == 'Kd' ").reset_index(drop=True)
