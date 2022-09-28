7b8068de6447f2c897b23dbca300c040c1fc381a
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-09-28 14:12:00 -0400

    sna option

--- datasets/bigbind.py
+++ datasets/bigbind.py
@@ -8,8 +8,13 @@ class BigBindDataset(CacheableDataset):
         super(BigBindDataset, self).__init__(cfg, name)
         self.cfg = cfg
         self.dir = cfg.platform.bigbind_dir
+        if cfg.data.sna_frac is not None:
+            act_split = f"sna_{cfg.data.sna_frac}_{split}"
+        else:
+            act_split = split
         all_structures = pd.read_csv(self.dir + f"/structures_{split}.csv")
-        all_activities = pd.read_csv(self.dir + f"/activities_{split}.csv")
+        all_activities = pd.read_csv(self.dir + f"/activities_{act_split}.csv")
+
         # can delete this once I remove the single atom mols in bigbind
         self.structures = all_structures
         self.activities = all_activities[all_activities.lig_smiles.str.len() > 5].reset_index(drop=True)
