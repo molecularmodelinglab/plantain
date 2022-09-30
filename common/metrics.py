1fbef2cba3489d1499c1bcdd82a31d968e48fc57
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-09-30 16:37:14 -0400

    lol metrics working now

--- common/metrics.py
+++ common/metrics.py
@@ -1,16 +1,19 @@
+import torch
 from torch import nn
 from torchmetrics import Metric, Accuracy, AUROC
 
 class MetricWrapper(Metric):
 
-    def __init__(self, metric, get_y):
+    def __init__(self, metric, get_y_p, get_y_t):
         super().__init__()
         self.metric = metric
-        self.get_y = get_y
+        self.get_y_p = get_y_p
+        self.get_y_t = get_y_t
 
     def update(self, pred, batch):
-        y_true = self.get_y(batch)
-        return self.metric.update(pred, y_true)
+        y_pred = self.get_y_p(pred)
+        y_true = self.get_y_t(batch)
+        return self.metric.update(y_pred, y_true)
 
     def compute(self):
         return self.metric.compute()
@@ -18,8 +21,8 @@ class MetricWrapper(Metric):
 def get_metrics(cfg):
     return {
         "classification": nn.ModuleDict({
-            "acc": MetricWrapper(Accuracy(), lambda b: b.is_active),
-            "bal_acc": MetricWrapper(Accuracy(average="macro", num_classes=2, multiclass=True), lambda b: b.is_active),
-            "auroc": MetricWrapper(AUROC(), lambda b: b.is_active)
+            "acc": MetricWrapper(Accuracy(), torch.sigmoid, lambda b: b.is_active),
+            "bal_acc": MetricWrapper(Accuracy(average="macro", num_classes=2, multiclass=True), torch.sigmoid, lambda b: b.is_active),
+            "auroc": MetricWrapper(AUROC(), torch.sigmoid, lambda b: b.is_active)
         })
     }[cfg.task]
