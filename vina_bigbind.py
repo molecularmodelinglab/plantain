1dc964d74c81b601f911959784844dea362bbaf6
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-11-04 22:32:54 -0400

    32 vina

--- vina_bigbind.py
+++ vina_bigbind.py
@@ -18,7 +18,30 @@ from tqdm import tqdm
 
 from common.cfg_utils import get_config
 
+def prepare_rec(cfg, rec_file):
+    """ Use ADFR to produce a pdbqt file from the receptor pdb file """
+    rec = cfg.platform.bigbind_dir + "/" + rec_file
+    rec_folder, imm_rec_file = rec.split("/")[-2:]
+    out_folder = cfg.platform.bigbind_vina_dir + "/" + rec_folder
+    out_file = out_folder + "/" + imm_rec_file + "qt"
+    
+    os.makedirs(out_folder, exist_ok=True)
+
+    if os.path.exists(out_file): return out_file
+    
+    prep_cmd = f"{cfg.platform.adfr_dir}/bin/prepare_receptor"
+    proc = subprocess.run([prep_cmd, "-r", rec, "-o", out_file])
+    try:
+        proc.check_returncode()
+        return out_file
+    except KeyboardInterrupt:
+        raise
+    except:
+        print_exc()
+        return None
+
 def move_lig_to_center(lig, center):
+    """ Ensure the ligand's conformer centroid is at center """
     center_pt = rdkit.Geometry.rdGeometry.Point3D(*center)
     conf = lig.GetConformer()
     lig_center = rdMolTransforms.ComputeCentroid(conf)
@@ -28,6 +51,7 @@ def move_lig_to_center(lig, center):
         conf.SetAtomPosition(i, new_pos)
 
 def get_lig_size(lig, padding=3):
+    """ Returns center and size of the molecule conformer """
     bounds = ComputeConfBox(lig.GetConformer(0))
     bounds_min = np.array([bounds[0].x, bounds[0].y, bounds[0].z])
     bounds_max = np.array([bounds[1].x, bounds[1].y, bounds[1].z])
@@ -35,29 +59,28 @@ def get_lig_size(lig, padding=3):
     size = (bounds_max - center + padding)*2
     return tuple(center), tuple(size)
 
-def vina_score_cached(cfg, i, row, lig_file, rec_file, exhaust=8):
+def run_vina(cfg, out_folder, i, row, lig_file, rec_file, exhaust=32):
     
     name = rec_file.split("/")[-1] + "_" + lig_file.split("/")[-1]
     center = (row.pocket_center_x, row.pocket_center_y, row.pocket_center_z)
     size = (row.pocket_size_x, row.pocket_size_y, row.pocket_size_z)
 
-    lig = next(Chem.SDMolSupplier(lig_file, sanitize=True))
-    move_lig_to_center(lig, center)
-    _, lig_size = get_lig_size(lig)
-    size = max(lig_size, size)
+    cache_folder = cfg.platform.cache_dir + "/run_vina/"
+    lig_pdbqt = cache_folder + name + "_lig.pdbqt"
+    if not os.path.exists(lig_pdbqt):
+        lig = next(Chem.SDMolSupplier(lig_file, sanitize=True))
+        move_lig_to_center(lig, center)
+        _, lig_size = get_lig_size(lig)
+        size = max(lig_size, size)
 
+        preparator = MoleculePreparation(hydrate=False)
+        preparator.prepare(lig)
+        os.makedirs(cache_folder, exist_ok=True)
+        preparator.write_pdbqt_file(lig_pdbqt)
 
-    preparator = MoleculePreparation(hydrate=False)
-    preparator.prepare(lig)
-    os.makedirs(cfg["cache_folder"], exist_ok=True)
-    lig_pdbqt = cfg["cache_folder"] + "/" + name + "_lig.pdbqt"
-    preparator.write_pdbqt_file(lig_pdbqt)
-
-    out_folder = cfg["docked_folder"] + "/val_screens/" + row.pocket
     out_file = out_folder + f"/{i}.pdbqt"
-    os.makedirs(out_folder, exist_ok=True)
 
-    cmd = [cfg["vina_exec"], "--receptor", rec_file, "--ligand", lig_pdbqt, "--exhaustiveness", str(exhaust), "--cpu", str(exhaust) ]
+    cmd = [cfg.platform.vina_exec, "--receptor", rec_file, "--ligand", lig_pdbqt, "--exhaustiveness", str(exhaust), "--cpu", str(exhaust) ]
     for c, s, ax in zip(center, size, ["x", "y", "z"]):
         cmd += ["--center_"+ax, str(c)]
         cmd += ["--size_"+ax, str(s)]
@@ -69,20 +92,19 @@ def vina_score_cached(cfg, i, row, lig_file, rec_file, exhaust=8):
     return out_file
 
 
-def get_vina_score(cfg, tup):
+def get_vina_score(cfg, out_folder, tup):
     i, row = tup
 
-    lig_file = cfg["bigbind_folder"] + "/" + row.lig_file
-    rec_file = cfg["docked_folder"] + "/" + row.ex_rec_file.split(".")[0] + ".pdbqt"
-    
-    name = rec_file.split("/")[-1] + "_" + lig_file.split("/")[-1]
+    lig_file = cfg.platform.bigbind_dir + "/" + row.lig_file
+    rec_file = prepare_rec(cfg, row.ex_rec_file)
+    if rec_file is None: return None
 
-    if not os.path.exists(lig_file) or not os.path.exists(rec_file):
-        return None
+    # if not os.path.exists(lig_file) or not os.path.exists(rec_file):
+    #     return None
     
     ret = None
     try:
-        ret = vina_score_cached(cfg, i, row, lig_file, rec_file)
+        ret = run_vina(cfg, out_folder, i, row, lig_file, rec_file)
     except KeyboardInterrupt:
         raise
     except:
@@ -94,15 +116,15 @@ def get_vina_score(cfg, tup):
 if __name__ == "__main__":
 
     cfg = get_config()
-    os.makedirs(cfg.platform.bigbind_vina_dir, exist_ok=True)
 
     for split in [ "train", "val", "test" ]:
+        out_folder = cfg.platform.bigbind_vina_dir + "/" + split
+        os.makedirs(out_folder, exist_ok=True)
+
         screen_csv = cfg.platform.bigbind_dir + f"/activities_sna_1_{split}.csv"
         screen_df = pd.read_csv(screen_csv)
-        print(screen_df)
-        continue
 
-        with Pool(processes=8) as p:
-            score_fn = partial(get_vina_score, cfg)
+        with Pool(processes=cfg.platform.vina_processes) as p:
+            score_fn = partial(get_vina_score, cfg, out_folder)
             for ret_file in p.imap_unordered(score_fn, screen_df.iterrows()):
                 print(ret_file)
