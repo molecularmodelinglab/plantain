8be53a0abb6e74e380232a5de072efe672313937
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-12-06 18:49:33 -0500

    try 5

--- datasets/graphs/interaction_graph.py
+++ datasets/graphs/interaction_graph.py
@@ -88,8 +88,8 @@ def merge_graphs(inter_cfg, g1: MolGraph, g2: ProtGraph) -> Tuple[List[Interacti
     edges += extra_edges
     edata = merge_batches([g1.edata, g2.edata, extra_edata], Edge3d)
 
-    assert edata.scal_feat.shape[-1] == 2
-    assert edata.cat_feat.shape[-1] == 1
+    # assert edata.scal_feat.shape[-1] == 2
+    # assert edata.cat_feat.shape[-1] == 1
 
     return nodes, edges, edata
 
