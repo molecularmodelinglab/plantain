ae8725f1d7d74fa47871c06f07bd1ae4403cf68a
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-12-06 17:52:20 -0500

    try 3

--- routines/ai_routine.py
+++ routines/ai_routine.py
@@ -88,6 +88,7 @@ class AIRoutine(pl.LightningModule):
                              val_check_interval=self.cfg.val_check_interval,
                              logger=logger,
                              callbacks=callbacks,
+                             replace_sampler_ddp=False,
                              resume_from_checkpoint=self.checkpoint_file)
         trainer.fit(self, self.train_dataloader, self.val_dataloader)
         trainer.validate(self, self.val_dataloader)
