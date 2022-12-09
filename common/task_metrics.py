cd0e3395c8137ba4f7bcbafbfe4e5b2653690d86
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-12-09 18:51:08 -0500

    forgot task

--- common/task_metrics.py
+++ common/task_metrics.py
@@ -13,7 +13,6 @@ def get_pose_acc(rmsds, cutoff):
     return sum(rmsds < cutoff)/len(rmsds)
 
 def get_pose_metrics(prefix, batch_idx, model, batch):
-    return {}
     # inference is expensive so don't do it all the time
     if prefix == "train" and batch_idx % 10 != 0:
         return {}
