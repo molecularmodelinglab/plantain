6156e0ed5b846a476e9643e18bf34e0058ec4593
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-01-31 09:06:52 -0500

    all the metrics!

--- analysis/reject_analysis.py
+++ analysis/reject_analysis.py
@@ -4,7 +4,7 @@ from common.wandb_utils import get_old_model
 from validation.validate import save_validation
 
 def main(cfg):
-    model_names = [ "bce_mse" ]
+    model_names = [ "pos_softmax_0.5", "softmax_0.001", "select_correct", "bce_mse",]# "combo_1.5_0.5" ]
     for name in model_names:
         print(f"Validating {name}")
         model = get_old_model(cfg, name, "latest")
