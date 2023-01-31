c179dc73ae62a645face5b0990c1afcdf9b14fd2
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-01-31 09:07:21 -0500

    all the metrics!

--- analysis/reject_analysis.py
+++ analysis/reject_analysis.py
@@ -8,7 +8,7 @@ def main(cfg):
     for name in model_names:
         print(f"Validating {name}")
         model = get_old_model(cfg, name, "latest")
-        save_validation(cfg, model, "bigbind_act", "val", 5000)
+        save_validation(cfg, model, "bigbind_act", "val", 250)
 
 if __name__ == "__main__":
     cfg = get_config("attention_gnn")
