6ee74c0934ede6462659781158dcc5333c61130f
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-02-02 15:29:26 -0500

    SVM uncertainty, no properly named

--- analysis/reject_screen.py
+++ analysis/reject_screen.py
@@ -3,6 +3,7 @@ import pickle
 from common.cfg_utils import get_config
 from common.wandb_utils import get_old_model
 from models.gp_uncertainty import GPUncertainty
+from models.rf_uncertainty import RFUncertainty
 from validation.validate import save_validation
 from datasets.lit_pcba import LitPcbaDataset
 
@@ -10,14 +11,14 @@ def main(cfg):
     model_names = [ "bce_mse" ]
     for name in model_names:
         model = get_old_model(cfg, name, "latest")
-        u_model = GPUncertainty(cfg, model)
+        u_model = RFUncertainty(cfg, model)
         u_model.fit("bigbind_act", "val", 500)
 
         out_folder =f"outputs/results/{u_model.cache_key}"
-        print(f"Saving gp plot to {out_folder}")
+        print(f"Saving svm plot to {out_folder}")
         os.makedirs(out_folder, exist_ok=True)
         fig = u_model.plot()
-        fig.savefig(f"{out_folder}/gp_plot.pdf")
+        fig.savefig(f"{out_folder}/svm_plot.pdf")
 
         dataset = "lit_pcba"
         for target in LitPcbaDataset.get_all_targets(cfg): 
