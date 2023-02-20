0c5986f26450f4b3a88bb20077b61e374333aad5
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-02-20 09:19:34 -0500

    AAAAAA SGD vs BFGS again

--- analysis/validate_pose.py
+++ analysis/validate_pose.py
@@ -5,6 +5,6 @@ from validation.validate import validate
 cfg = get_config("diffusion")
 cfg.batch_size=1
 model = get_old_model(cfg, "intra_lig_energy", "best_k")
-metrics, plots = validate(cfg, model, cfg.val_datasets[0], "val", 50)
+metrics, plots = validate(cfg, model, cfg.val_datasets[0], "val", None)
 for key, val in metrics.items():
     print(f"{key}: {val:.3f}")
