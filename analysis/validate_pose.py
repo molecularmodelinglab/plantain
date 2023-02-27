f9b68edff65d3370ad3d8f6705833588050af2d1
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-02-27 09:14:59 -0500

    thiccer gnina ff

--- analysis/validate_pose.py
+++ analysis/validate_pose.py
@@ -3,10 +3,10 @@ from common.wandb_utils import get_old_model
 from models.sym_diffusion import SymDiffusion
 from validation.validate import validate
 
-cfg = get_config("diff_combo")
+cfg = get_config("diffusion")
 # cfg.batch_size=1
 # model = SymDiffusion(cfg)
-model = get_old_model(cfg, "combo", "latest")
+model = get_old_model(cfg, "combo", "v2")
 metrics, plots = validate(cfg, model, cfg.val_datasets[0], "val", None)
 for key, val in metrics.items():
     print(f"{key}: {val:.3f}")
