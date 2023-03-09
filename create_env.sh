d79051d8383e1c0abb223199152a6a37c70332ff
Michael Brocidiacono <mixarcidiacono@gmail.com>
2023-03-09 14:34:04 +0000

    env update

--- create_env.sh
+++ create_env.sh
@@ -4,3 +4,5 @@ mamba install pytorch==1.12.0 cudatoolkit=11.6 -c pytorch -c conda-forge -y
 # mamba install -c dglteam dgl-cuda11.6 -y
 pip install dgl-cu116 dglgo -f https://data.dgl.ai/wheels/repo.html
 pip install -r requirements.txt
+# this last line is sometimes necessary
+# pip uninstall nvidia_cublas_cu11
